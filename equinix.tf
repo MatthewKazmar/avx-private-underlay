ae37b58d9564b5dbe2b505b6c8dd1ee939b8ad89
Matt Kazmar <mkazmar@aviatrix.com>
2023-03-08 23:42:32 -0600

    local

--- equinix.tf
+++ equinix.tf
@@ -26,7 +26,7 @@ resource "equinix_ecx_l2_connection" "this" {
   count = local.is_azure == 1 ? 1 : local.is_redundant ? 2 : 1
 
   name                = "${var.circuit["circuit_name"]}-${count.index + 1}"
-  profile_uuid        = data.equinix_ecx_l2_sellerprofile.profiles[sellerprofile_map[local.cloud][count.index]]
+  profile_uuid        = data.equinix_ecx_l2_sellerprofile.profiles[local.sellerprofile_map[local.cloud][count.index]]
   speed               = var.circuit["speed_in_mbit"]
   speed_unit          = "MB"
   notifications       = var.circuit["notifications"]
