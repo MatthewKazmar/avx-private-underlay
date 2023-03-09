868de47600680e6340554b2b17e33a4401f5ef7e
Matt Kazmar <mkazmar@aviatrix.com>
2023-03-09 01:01:28 -0600

    one more

--- gcp.tf
+++ gcp.tf
@@ -19,7 +19,7 @@ resource "google_compute_interconnect_attachment" "primary" {
 }
 
 resource "google_compute_interconnect_attachment" "secondary" {
-  count = var.circuit["cloud_type"] == 8 && local.is_redundant ? 1 : 0
+  count = var.circuit["cloud_type"] == 8 && (length(compact(var.circuit["edge_uuid"])) == 2 || length(compact(var.circuit["metal_service_tokens"])) == 2) ? true : false ? 1 : 0
 
   name                     = "${var.circuit["circuit_name"]}-2"
   edge_availability_domain = "AVAILABILITY_DOMAIN_2"
