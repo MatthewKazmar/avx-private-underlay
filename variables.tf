b0c942934291e81b2382e833008c5b03b41577fb
Matt Kazmar <mkazmar@aviatrix.com>
2023-03-11 10:41:40 -0600

    maybe this time

--- variables.tf
+++ variables.tf
@@ -1,5 +1,6 @@
 variable "circuit" {
   type = object({
+    is_redundant                   = bool,
     circuit_name                   = string,
     cloud_type                     = number,
     vpc_id                         = string,
@@ -12,26 +13,10 @@ variable "circuit" {
     speed_in_mbit                  = string,
     edge_uuid                      = optional(list(string), [null, null]),
     edge_interface                 = optional(list(number), [null, null]),
-    metal_service_tokens           = list(string),
-#    metal_service_tokens           = optional(list(string), [null, null]),    
+    metal_service_tokens           = optional(list(string), [null, null]),
     equinix_side_asn               = number,
     vpc_asn                        = optional(number, 64512),
     bgp_auth_key                   = optional(string, "aviatrix1234#!"),
     notifications                  = list(string)
   })
-}
-
-locals {
-  is_redundant = {
-    #is_redundant = length(compact(var.circuit["edge_uuid"])) == 2 || length(compact(var.circuit["metal_service_tokens"])) == 2 ? true : false
-    is_redundant = length(compact(var.circuit["metal_service_tokens"])) == 2 ? true : false
-  }
-
-  circuit = merge(var.circuit, local.is_redundant)
-
-  cloud_map = {
-    "1" = "aws",
-    "8" = "azure",
-    "4" = "gcp"
-  }
 }
