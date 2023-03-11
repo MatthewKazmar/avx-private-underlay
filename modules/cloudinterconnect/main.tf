b67f1779aa339d846cf765edc32221819435bed0
Matt Kazmar <mkazmar@aviatrix.com>
2023-03-11 17:27:11 -0600

    interface issues

--- modules/cloudinterconnect/main.tf
+++ modules/cloudinterconnect/main.tf
@@ -25,16 +25,16 @@ resource "google_compute_interconnect_attachment" "this" {
   mtu                      = 1500
 }
 
-resource "google_compute_router_interface" "this" {
-  count = var.circuit["is_redundant"] ? 2 : 1
+# resource "google_compute_router_interface" "this" {
+#   count = var.circuit["is_redundant"] ? 2 : 1
 
-  project                 = local.project
-  name                    = "${google_compute_interconnect_attachment.this[count.index].name}-interface"
-  region          = google_compute_router.this.region
-  router                  = google_compute_router.this.name
-  ip_range                = google_compute_interconnect_attachment.this[count.index].cloud_router_ip_address
-  interconnect_attachment = google_compute_interconnect_attachment.this[count.index].self_link
-}
+#   project                 = local.project
+#   name                    = "${google_compute_interconnect_attachment.this[count.index].name}-interface"
+#   region          = google_compute_router.this.region
+#   router                  = google_compute_router.this.name
+#   ip_range                = google_compute_interconnect_attachment.this[count.index].cloud_router_ip_address
+#   interconnect_attachment = google_compute_interconnect_attachment.this[count.index].self_link
+# }
 
 resource "google_compute_router_peer" "this" {
   count = var.circuit["is_redundant"] ? 2 : 1
@@ -44,7 +44,7 @@ resource "google_compute_router_peer" "this" {
   region          = google_compute_router.this.region
   peer_ip_address = google_compute_interconnect_attachment.this[count.index].customer_router_ip_address
   peer_asn        = var.circuit["customer_side_asn"]
-  interface       = google_compute_router_interface.this[count.index].name
+  interface       = "interface-1"
   router          = google_compute_router.this.name
 }
 
