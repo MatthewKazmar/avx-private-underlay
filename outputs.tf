39c50a14dec03a859b639b469e9b4f776f4afd58
Matt Kazmar <mkazmar@aviatrix.com>
2023-03-11 11:15:09 -0600

    fmt

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "csp_peering_addresses" {
   description = "CSP side peering addresses."
-  value = local.module_output.csp_peering_addresses
+  value       = local.module_output.csp_peering_addresses
 }
 
 output "equinix_peering_addresses" {
   description = "Equinix side peering addresses."
-  value = local.module.equinix_peering_addresses
+  value       = local.module.equinix_peering_addresses
 }
