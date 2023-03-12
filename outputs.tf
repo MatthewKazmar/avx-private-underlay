4c676afc789c8d6e4d9b8d1073afe494ed974c34
Matt Kazmar <mkazmar@aviatrix.com>
2023-03-12 12:45:53 -0500

    outputs

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "csp_peering_addresses" {
   description = "CSP side peering addresses."
-  value       = local.module_output.csp_peering_addresses
+  value       = try(local.module_output.csp_peering_addresses, [])
 }
 
 output "customer_side_peering_addresses" {
   description = "Customer (Equinix Metal/Edge/Colo) peering addresses."
-  value       = local.module_output.customer_side_peering_addresses
+  value       = try(local.module_output.customer_side_peering_addresses, [])
 }
 
 output "customer_side_vlan_tags" {
   description = "Customer (Equinix Metal/Edge/Colo) vlans."
-  value       = local.module_output.customer_side_vlan_tags
+  value       = try(local.module_output.customer_side_vlan_tags, [])
 }
